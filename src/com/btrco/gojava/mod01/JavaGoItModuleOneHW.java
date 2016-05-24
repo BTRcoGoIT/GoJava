6eae9dc8c75af774363286b2f1cc689dbe52d8d2
unknown <bogdans@WS-BOGDANSK-7.corp.neogames-tech.com>
2016-05-24 10:39:35 +0300

    Initial commit and HW1

---
+++ src/com/btrco/gojava/mod01/JavaGoItModuleOneHW.java
@@ -0,0 +1,11 @@
+package com.btrco.gojava.mod01;
+
+public class JavaGoItModuleOneHW {
+	
+	public static void main(String[] args){
+	
+		System.out.println("Hi, I'm your first Java program! Stay tuned!");
+	
+	}
+
+}
